0909e7cb85188587f4fc75351b266e9f0b624fa0
puja2504 <114426819+puja2504@users.noreply.github.com>
2022-11-16 22:26:44 +0530

    Add files via upload

---
+++ read vedio.py
@@ -0,0 +1,23 @@
+import cv2
+video = cv2.VideoCapture("dog.mp4")
+if (video.isOpened() == False): 
+    print("Error reading video file")
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+ 
+size = (frame_width, frame_height)
+result = cv2.VideoWriter('out.mp4', cv2.VideoWriter_fourcc(*'MJPG'),10, size)
+ 
+while(True):
+    ret, frame = video.read()
+    if ret == True: 
+        result.write(frame)
+        cv2.imshow('Frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            break
+    else:
+        break
+video.release()
+result.release()
+cv2.destroyAllWindows()  
+print("The video was successfully saved")
